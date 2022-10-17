baeb801ba292b5e7bf8c6b4a8b9fbd8808b36253
aaakshit <105563106+aaakshit@users.noreply.github.com>
2022-10-18 00:03:27 +0530

    Time: 14 ms (70.49%), Space: 7.5 MB (13.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        for(auto x:s)
+        {
+            m[x]++;
+        }
+        for(auto x:t)
+        {
+            m[x]--;
+        }
+        
+        for(auto x:m)
+        {
+            if(x.second !=0)
+                return false;
+        
+        }
+        return true;
+    }
+};

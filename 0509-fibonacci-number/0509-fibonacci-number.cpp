b4bf28ad604d4887565bf54547a94b918370d37f
aaakshit <105563106+aaakshit@users.noreply.github.com>
2022-11-15 01:49:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.50%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,12 +4,18 @@ public:
     int fib(int n) {
         if(n==0)
             return 0;
-        int dp[n+1];
-        dp[0]=0;
-        dp[1]=1;
+        if(n==1)
+            return 1;
+        //int dp[n+1];
+        int dp1=0;
+        int dp2=1;
+        int sum=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            sum=dp1+dp2;
+            dp1=dp2;
+            dp2=sum;
         }
-        return dp[n];
+        
+        return sum;
     }
 };

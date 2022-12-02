d39fb4eb1f9479011f8052952ead56df1eda955e
aaakshit <105563106+aaakshit@users.noreply.github.com>
2022-12-02 16:57:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int dp[105][105];
-    int solve(int m, int n, int row ,int column){
-        if(row==m && column==n)
+    int solve(int m,int n, int i, int j){
+        if(i==m && j==n)
             return 1;
-        if(row>m || column>n)
+        if(i>m || j>n)
             return 0;
-        if(dp[row][column]!=-1)
-            return dp[row][column];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=(solve(m,n,i+1,j)+solve(m,n,i,j+1));
         
-        return dp[row][column]=(solve(m,n,row+1,column)+solve(m,n,row,column+1));
     }
     int uniquePaths(int m, int n) {
         memset(dp,-1,sizeof(dp));

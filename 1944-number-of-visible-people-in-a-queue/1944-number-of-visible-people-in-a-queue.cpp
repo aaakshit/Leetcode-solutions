80d2411b152acf537fb1ef274166a25b76a70475
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-01-12 22:10:25 +0530

    Time: 177 ms (97.42%), Space: 84.1 MB (56.70%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-   vector<int> canSeePersonsCount(vector<int>& heights) {
-    vector<int> mono;
-    for (int i = heights.size() - 1; i >= 0; --i) {
-        int h = heights[i], cnt = 0;
-        for (; !mono.empty() && mono.back() < h; ++cnt)
-            mono.pop_back();
-        heights[i] = cnt + !mono.empty();
-        mono.push_back(h);
+   vector<int> canSeePersonsCount(vector<int>& h) {
+         int n=h.size();
+        vector<int>ans(n,0);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--)  {
+            int cnt=0;
+                while(!st.empty() && st.top()<h[i])   {
+                    st.pop();
+                    cnt++;
+                }
+            ans[i]=st.empty()?cnt:cnt+1;
+            st.push(h[i]);
+        }
+        return ans;
     }
-    return heights;
-}
 };

683d29f8655eec0791a9af0d7475fd8fa1476af5
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-01 01:11:05 +0530

    Time: 15 ms (37.07%), Space: 14.3 MB (27.64%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int oldcolor;
-    void solve(vector<vector<int>>& image, int i, int j, int color){
-        if(i<0 || i>=image.size()||j<0 || j>=image[0].size()||image[i][j]!=oldcolor || image[i][j]==color)
+    int c,oc;
+    void solve(int i, int j , vector<vector<int>>&img,vector<vector<bool>>&vis){
+        if(i<0 || j<0 || i>=img.size()|| j>=img[0].size() || img[i][j]==c||img[i][j]!=oc)
             return;
-        image[i][j]=color;
-         solve(image,i+1,j,color);
-         solve(image,i-1,j,color);
-         solve(image,i,j+1,color);
-         solve(image,i,j-1,color);
-        
+      //  vis[i][j]=1;
+        img[i][j]=c;
+        solve(i+1,j,img,vis);
+        solve(i,j+1,img,vis);
+        solve(i-1,j,img,vis);
+        solve(i,j-1,img,vis);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        oldcolor=image[sr][sc];
-        solve(image,sr,sc,color);
+        vector<vector<bool>>vis(image.size(),vector<bool>(image.size(),0));
+        c=color;
+        oc=image[sr][sc];
+        solve(sr,sc,image,vis);
         return image;
     }
 };

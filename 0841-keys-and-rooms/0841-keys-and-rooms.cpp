e9a476cb3b6576ca7c35f6a7c0b62642b1fc6073
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-01 03:17:24 +0530

    Time: 7 ms (95.89%), Space: 10.6 MB (31.27%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    void dfs(int i,vector<vector<int>>&rooms,vector<int>&vis){
-        vis[i]=1;
-        for(auto x:rooms[i]){
-            if(!vis[x])
-                dfs(x,rooms,vis);
+    void solve(int i,vector<vector<int>>& rooms,vector<int>&vis){
+        if(!vis[i]){
+            vis[i]=1;
+            for(auto x:rooms[i]){
+                solve(x,rooms,vis);
+            }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<int>vis(rooms.size(),0);
-        dfs(0,rooms,vis);
-        for(auto x:vis){
-            if(!x)
+        int n=rooms.size();
+        int m=rooms[0].size();
+        vector<int>vis(n,0);
+        solve(0,rooms,vis);
+        for(int i=0;i<n;i++){
+            if(!vis[i])
                 return 0;
         }
         return 1;

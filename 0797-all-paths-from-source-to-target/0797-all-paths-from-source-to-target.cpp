e8b37a09b01b757fe6a62657e476f45aeed00d07
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-01 03:04:00 +0530

    Time: 18 ms (51.46%), Space: 10.6 MB (92.92%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-   vector<vector<int>> ans;
-    void dfs(int node, vector<int> &path, vector<vector<int>> &adj)
-    {
-        // reached destination, push current path to the answer
-        if (node == adj.size() - 1)
-        {
-            ans.push_back(path);
+    void solve(int i,vector<vector<int>>& graph,vector<int>& v,vector<vector<int>>& ans){
+        if(i==graph.size()-1){
+            ans.push_back(v);
             return;
         }
-
-        // explore all path from the current node
-        for (auto i : adj[node])
-        {
-            // push current child to the path (if it will reach the destination then base case will take care of it)
-            path.push_back(i);
-            dfs(i, path, adj);
-            // backtrack
-            path.pop_back();
+        for(auto x:graph[i]){
+            v.push_back(x);
+            solve(x,graph,v,ans);
+            v.pop_back();
         }
     }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph)
-    {
-        ans.clear();
-        vector<int> path = {0};
-        dfs(0, path, graph);
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+       vector<vector<int>>ans;
+        vector<int>v;
+        v.push_back(0);
+        solve(0,graph,v,ans);
         return ans;
     }
 };

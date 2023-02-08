3c821eeb7391906770b12d8dc69b9111ee86d5df
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-08 11:33:10 +0530

    Time: 475 ms (24.47%), Space: 18.1 MB (6.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int ans=INT_MAX;
-    int solve(vector<int>& nums,vector<int>&dp,int i){
+    vector<int>dp;
+    int solve(int i,vector<int>&nums){
         if(i>=nums.size()-1)
             return 0;
-        if(dp[i]!=10005)
+        if(dp[i]!=-1)
             return dp[i];
+        int ans=10005;
         for(int j=1;j<=nums[i];j++){
-            dp[i]=min(dp[i],1+solve(nums,dp,i+j));
+            ans=min(ans,solve(i+j,nums)+1);
         }
-        return dp[i];
+        return dp[i]=ans;
     }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),10005);
-        return solve(nums,dp,0);
+        if(nums[0]==0)
+            return 0;
+        dp.resize(nums.size()+5,-1);
+        return solve(0,nums);
     }
 };

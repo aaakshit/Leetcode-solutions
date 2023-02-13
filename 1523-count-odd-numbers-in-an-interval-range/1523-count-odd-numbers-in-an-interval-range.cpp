be949c2b953b0b979c339f5fc4856ac0da9251f7
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-13 23:40:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        if(!(high-low+1)&1)
-            return (high-low+1)/2;
-        else{
-        if(low&1 && high&1)
-             return ((high-low+1)/2+1);
-        else
-           return (high-low+1)/2;
+        int ans1=0,ans2=0;
+        if(high%2){
+            ans1+=(high/2)+1;
         }
-            
+        if(low%2){
+            ans2+=(low/2)+1;
+        }
+        if(high%2==0){
+            ans1+=high/2;
+        }
+        if(low%2==0){
+            ans2+=low/2;
+        }
+        cout<<ans2<<" "<<ans1<<endl;
+        if(low%2)
+        return (ans1-ans2)+1;
+        else
+            return (ans1-ans2);
     }
 };

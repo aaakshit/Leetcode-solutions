8fdc3e8a47633dfa7e4cc4dc30f23d7ea2722299
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-02-20 01:53:22 +0530

    Time: 34 ms (54.73%), Space: 13.8 MB (42.97%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,8 +1,20 @@
 class Solution {
- const int inf = 1e8;
-    int dp[101][101];
-
 public:
+    int dp[101][101];
+    const int inf = 1e8;
+    int solve(int s,int k,int d,vector<vector<int>>&cost,vector<int>adj[]){
+        if(k<0)
+            return inf;
+        if(s==d)
+            return 0;
+        if(dp[s][k]!=-1)
+            return dp[s][k];
+        int ans=inf;
+        for(auto x:adj[s]){
+            ans=min(ans,cost[s][x]+solve(x,k-1,d,cost,adj));
+        }
+        return dp[s][k]=ans;
+    }
     int dfs(int node, int k, int dest, vector<vector<int>> &cost, vector<int> adj[])
     {
         if (k < 0)
@@ -20,19 +32,17 @@ public:
 
         return dp[node][k] = ans;
     }
-    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
-    {
-        memset(dp, -1, sizeof dp);
-
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
         vector<vector<int>> cost(n, vector<int>(n));
         vector<int> adj[n];
-        for (auto e : flights)
+        for (auto e : f)
         {
             adj[e[0]].push_back(e[1]);
             cost[e[0]][e[1]] = e[2];
         }
-
-        int ans = dfs(src, k + 1, dst, cost, adj);
-        return ans == inf ? -1 : ans;
+        memset(dp,-1,sizeof(dp));
+        int ans=solve(src,k+1,dst,cost,adj);
+        return ans==inf?-1:ans;
+       
     }
 };

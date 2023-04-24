308f04343118cb5e1524f2a471140be2a427d9ad
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-04-24 20:08:48 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (84.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int>pq(stones.begin(),stones.end());
+        
+        priority_queue<int>pq;
+        for(auto x:stones){
+            pq.push(x);
+        }
         while(pq.size()>1){
             int x=pq.top();
             pq.pop();
             int y=pq.top();
             pq.pop();
-            if(x!=y)
+            if(x!=y){
                 pq.push(x-y);
+            }
         }
-        return pq.empty()?0:pq.top();
-       //  for(int i=0;i<stones.size();i++){
-       //      pq.push(stones[i]);
-       //  }
-       // // cout<<pq.size()<<endl;
-       //  if(pq.size()==2){
-       //      int x=pq.top();
-       //      pq.pop();
-       //      return x-pq.top();
-       //  }
-       //  while(pq.size()>2){
-       //      int x=pq.top();
-       //      pq.pop();
-       //      pq.push(x-pq.top());
-       //       cout<<pq.top()<<endl;
-       //      pq.pop();
-       //  }
-       //  return pq.top();
+        if(!pq.empty())
+        return pq.top();
+        return 0;
     }
 };

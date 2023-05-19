e05db1fb6180c01c3cc09b07498aa64aa32a2902
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-05-20 02:08:37 +0530

    Time: 28 ms (49.50%), Space: 14.6 MB (16.88%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,39 @@
 class Solution {
-public:
-    bool solve(vector<vector<int>>& adj,vector<int>&vis,int c,int i){
-         vis[i]=c;
-        for(auto x:adj[i]){
-            if(!vis[x]){
-            if(solve(adj,vis,-c,x))
-                return 1;
-            }
-            else if(vis[x]==c)
-                return 1;
-        }
-        return false;
+private :
+    bool bfs(int start,vector<int>& color , vector<vector<int>>& graph){
+
+          queue<int> q;
+          q.push(start);
+
+          while(!q.empty()){
+
+              int node = q.front();
+              q.pop();
+
+              for(auto it:graph[node]){
+
+                  if(color[it] == -1){
+                      color[it] = !color[node];
+                      q.push(it);
+                  }
+                  else if(color[it] == color[node])
+                   return false;
+              }
+          }
+
+        return true;  
     }
+public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        int m=graph[0].size();
-       // vector<int>adj[n];
-        vector<int>vis(n,0);
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         adj[graph[i][j]].push_back(graph[j][i]);
-        //         adj[graph[j][i]].push_back(graph[i][j]);
-        //     }
-        // }
-        for(int i=0;i<n;i++){
-            if(!vis[i])
-            if(solve(graph,vis,1,i))
+
+          int n = graph.size();
+          vector<int> color(n,-1);
+
+           for(int i=0;i<n;i++){
+               if(!bfs(i,color,graph))
                 return false;
-        }
+           }
+
         return true;
     }
 };

d70e9cee521f04ccffd158b8a290e4e2ddc1383b
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-05-30 00:04:17 +0530

    Time: 60 ms (74.17%), Space: 33.1 MB (60.01%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,12 +1,38 @@
 class ParkingSystem {
 public:
-    vector<int> count;
+    int one=1,two=1,three=1,b,m,s;
     ParkingSystem(int big, int medium, int small) {
-        count = {big, medium, small};
+        b=big;
+        m=medium;
+        s=small;
     }
-
-    bool addCar(int carType) {
-        return count[carType - 1]-- > 0;
+    
+    bool addCar(int c) {
+        if(c==1){
+            if(one<=b){
+                one++;
+                return 1;
+            }
+            else
+                return 0;
+        }
+        if(c==2){
+            if(two<=m){
+                two++;
+                return 1;
+            }
+            else
+                return 0;
+        }
+        if(c==3){
+            if(three<=s){
+                three++;
+                return 1;
+            }
+            else
+                return 0;
+        }
+        return 0;
     }
 };
 

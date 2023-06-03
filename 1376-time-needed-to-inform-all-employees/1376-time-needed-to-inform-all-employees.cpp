7aca3234bfee389754eaf73acb0ba18e957f3b9e
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-06-03 23:55:09 +0530

    Time: 347 ms (40.68%), Space: 112.7 MB (82.82%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-	void dfs(int headID, vector<int>& informTime, int ans, int& maxi, unordered_map<int, vector<int>>& adj){
-		maxi=max(maxi, ans);
-		for(auto it: adj[headID]){
-			ans+=informTime[headID];
-			dfs(it, informTime, ans, maxi, adj);
-			ans-=informTime[headID];
-		}
-	}
-	int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-		unordered_map<int, vector<int>>adj;
-		int ans=0, maxi=0;
-		for(int i=0; i<manager.size(); i++){
-			if(manager[i]!=-1){
-				adj[manager[i]].push_back(i);
-			}
-		}
-		dfs(headID, informTime, ans, maxi, adj);
-		return maxi;
-	}
+    int dfs(int headId,vector<int>& informTime,vector<int> adj[]){
+
+        int maxTime = 0;
+
+        for(auto it:adj[headId])
+           maxTime = max(maxTime,dfs(it,informTime,adj));
+
+        return informTime[headId] + maxTime;   
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+            vector<int> adj[100005];
+
+            for(int i=0;i<manager.size();i++)
+                if(manager[i] != -1)
+                  adj[manager[i]].push_back(i);
+            
+        return dfs(headID,informTime,adj);    
+    }
 };

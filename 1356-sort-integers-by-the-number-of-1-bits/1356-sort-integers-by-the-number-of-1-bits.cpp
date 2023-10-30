d935dfd2bc60715dea339b1a79377ee6b6af494e
aaakshit <105563106+aaakshit@users.noreply.github.com>
2023-10-30 21:26:52 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (81.00%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,72 +1,13 @@
 class Solution {
 public:
-    
-    static bool cmp(const pair<int,int> &a, const pair<int,int> &b)
-    {
-        if(a.second==b.second)
-            return (a.first < b.first);
-        
-        return (a.second < b.second);
+    static bool cmp(int&a,int&b){
+        int x=__builtin_popcount(a);
+        int y=__builtin_popcount(b);
+        if(x==y)return a<=b;
+        return x<y;
     }
-    
-    int convert_binary(int n)
-    {
-    vector<int>arr;
-    while(n >= 1)
-    {
-        arr.push_back(n%2);
-        n/=2;
-    }
-
-    reverse(arr.begin(),arr.end());
-
-    //return arr;
-        int count=0;
-        for(auto x:arr)
-        {
-            if(x==1)
-                count++;
-        }
-        
-        return count;
-    }
-    
-   
-    
-    
     vector<int> sortByBits(vector<int>& arr) {
-     
-        //sort(arr.begin(),arr.end());
-        
-        vector<pair<int,int>>nums;
-        
-        for(int i=0;i<arr.size();i++)
-        {
-            int ele=convert_binary(arr[i]);
-            nums.push_back(make_pair(arr[i],ele));
-        }
-        
-        // for(auto x:nums)
-        // {
-        //     cout<<nums.first<<" "<<nums.second<<endl;
-        // }
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     cout<<nums[i].first<<" "<<nums[i].second<<endl;
-        // }
-        
-        sort(nums.begin(),nums.end(), cmp);
-        
-        //  for(int i=0;i<nums.size();i++)
-        // {
-        //     cout<<nums[i].first<<" "<<nums[i].second<<endl;
-        // }
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            arr[i]=nums[i].first;
-        }
-        
+        sort(arr.begin(),arr.end(),cmp);
         return arr;
     }
 };

43c0ca9eda2ef9570c9e1ac046565b44cc04f06e
aaakshit <105563106+aaakshit@users.noreply.github.com>
2024-01-29 18:56:11 +0530

    Time: 6 ms (64.28%), Space: 7.7 MB (89.65%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int dp[51][51][51];
+    int solve(int i,int j,int m,int n,int mm){
+        if(i>=m || i<0 || j>=n || j<0 ){
+            if(mm>=0)
+                return 1;
+            return 0;
+        }
+        if(mm<0)
+            return 0;
+        if(dp[i][j][mm]!=-1)
+            return dp[i][j][mm];
+        int ans=0;
+        ans=(ans+solve(i+1,j,m,n,mm-1))%mod;
+        ans=(ans+solve(i,j+1,m,n,mm-1))%mod;
+        ans=(ans+solve(i-1,j,m,n,mm-1))%mod;
+        ans=(ans+solve(i,j-1,m,n,mm-1))%mod;
+        dp[i][j][mm]=ans%mod;
+        return ans;
+        
+    }
+    int findPaths(int m, int n, int mm, int i, int j) {
+        memset(dp,-1,sizeof(dp));
+        return solve(i,j,m,n,mm);
+    }
+};
